284089049e0a5752a826cfca6512581854e517fe
Lita Li <litalidev@gmail.com>
2021-11-29 15:39:30 +0800

    Committing main.c on 2021-11-29_153921

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
 int main(){
-  fprintf(stdout, "Hello World, today is 2021-11-24 15:39:13\n");
+  fprintf(stdout, "Hello World, today is 2021-11-29 15:39:21\n");
   return 0;
 }
